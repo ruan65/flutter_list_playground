7f6662c8c0dd61384ba0ea776c94f74f1b6ea306
ruan65 <ruan65@gmail.com>
2019-09-08 13:43:04 +0300

    fix

--- lib/animation_training/music_album_animation.dart
+++ lib/animation_training/music_album_animation.dart
@@ -10,24 +10,27 @@ int ms2 = 2000;
 
 class _MusicAlbumAnimationState extends State<MusicAlbumAnimation>
     with TickerProviderStateMixin {
-  Animation<double> _animation1;
+  Animation<double> _animation;
   AnimationController _controller1;
   AnimationController _controller2;
 
   @override
   void initState() {
     super.initState();
-//    _controller1 =
-//        AnimationController(vsync: this, duration: Duration(milliseconds: ms));
-//    _controller2 =
-//        AnimationController(vsync: this, duration: Duration(milliseconds: ms2));
-//
-//    _animation1 = Tween(begin: 0.5, end: 1).animate(CurvedAnimation(
-//        parent: CurvedAnimation(parent: _controller2, curve: Curves.easeInOut),
-//        curve: null));
-//
-//    _controller2.forward();
-//    _controller1.repeat();
+    _controller1 =
+        AnimationController(vsync: this, duration: Duration(milliseconds: ms));
+    _controller2 =
+        AnimationController(vsync: this, duration: Duration(milliseconds: ms2));
+
+    _animation = Tween(begin: 0.0, end: 0.5).animate(
+        CurvedAnimation(parent: _controller2, curve: Curves.easeInOut));
+
+    _animation.addListener(() {
+      setState(() {});
+    });
+
+    _controller2.forward();
+    _controller1.repeat();
   }
 
   @override
@@ -48,8 +51,11 @@ class _MusicAlbumAnimationState extends State<MusicAlbumAnimation>
       fit: StackFit.expand,
       children: <Widget>[
         Opacity(
-          opacity: 0.5,
-          child: Image.asset('assets/mic.jpeg', fit: BoxFit.cover,),
+          opacity: _animation.value,
+          child: Image.asset(
+            'assets/mic.jpeg',
+            fit: BoxFit.cover,
+          ),
         )
       ],
     );
